b95daffeab9790f88407681529a5680d742e1059
keyboardbreaker <yujin_1991@hotmail.com>
2026-07-17 02:46:59 +0100

    feat(console): wire repository and service manually

--- PropertyRent.Console/Program.cs
+++ PropertyRent.Console/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.DependencyInjection;
+using PropertyRent.Application.Interfaces;
+using PropertyRent.Application.Services;
+using PropertyRent.Domain.Entities;
+using PropertyRent.Infrastructure.Repositories;
 
-Console.WriteLine("Hello, World!");
+var services = new ServiceCollection();
+
+services.AddSingleton<IPropertyRepository, PropertyRepository>();
+
+services.AddSingleton<PropertyService>();
+
+var provider = services.BuildServiceProvider();
+
+var service = provider.GetRequiredService<PropertyService>();
+
+service.Create(new Property
+{
+    Id = 1,
+    Address = "generic street address",
+    MonthlyRent = 2500,
+    IsOccupied = true,
+});
+
+var properties = service.GetAll();
+
+foreach (var property in properties)
+{
+    Console.WriteLine($"{property.Address} - £{property.MonthlyRent}");
+}
+
+var expensiveProperties = properties
+    .Where(p => p.MonthlyRent > 2000)
+    .OrderByDescending(p => p.MonthlyRent);
+
+foreach (var property in expensiveProperties)
+{
+    Console.WriteLine(property.Address);
+}
